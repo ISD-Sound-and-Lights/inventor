2334b0dfb40ee6de1cb5717d2b76b2577b984793
DyingEcho <theo.court@yahoo.com>
2017-12-05 19:52:06 +0100

    Improve header

--- header.py
+++ header.py
@@ -1,14 +1,20 @@
-import htmlify
+from htmlify import *
+
+print("Content-Type: text/html;charset=utf-8")
+print()
+
+# head
+dispHTML("head", contents=getHTML("script", src="https://use.fontawesome.com/344eca865b.js"))
 
 def showHeader():
 	# Title and menu
-	title = htmlify.getHTML("h1", "InventoryControl")  # title
-	menuItemHome = htmlify.getHTML("li", contents=htmlify.getHTML("a", contents="Home", href="/"))  # menu item 1 -- Home
-	menuItemHelp = htmlify.getHTML("li", contents=htmlify.getHTML("a", contents="Help", href="/help"))  # menu item 2 -- Help
-	menuItemLogin = htmlify.getHTML("li", contents=htmlify.getHTML("a", contents="Login", href="/login"))  # menu item 3 -- Login
+	title = getHTML("h1", "InventoryControl")  # title
+	menuItemHome = getHTML("li", contents=getHTML("a", contents="Home", href="/"))  # menu item 1 -- Home
+	menuItemHelp = getHTML("li", contents=getHTML("a", contents="Help", href="/help"))  # menu item 2 -- Help
+	menuItemLogin = getHTML("li", contents=getHTML("a", contents="Login", href="/cgi-bin/ic/login.py"))  # menu item 3 -- Login
 	menu = menuItemHome + menuItemHelp + menuItemLogin  # construct a menu but don't output yet
-	menu = htmlify.getHTML("div", contents=menu, id="menu")  # put menu into a div id="menu"
+	menu = getHTML("div", contents=menu, id="menu")  # put menu into a div id="menu"
 	header = title + menu  # get a header; don't oput yet.
-	htmlify.dispHTML("div", contents=header, id="header")  # put header into div and oput
-	htmlify.dispHTML("hr")
-	htmlify.dispHTML("br")
+	dispHTML("div", contents=header, id="header")  # put header into div and oput
+	dispHTML("hr")
+	dispHTML("br")
